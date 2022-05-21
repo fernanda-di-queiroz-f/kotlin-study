37ade47b6115769ff4a1a967b6127126014733af
Fernanda Di Queiroz Freitas <fernanda.diqueirozfreitas@thomsonreuters.com>
2022-05-21 17:47:35 -0300

    Challeng 1 adjustments

--- src/main/kotlin/StudyChallenge1.kt
+++ src/main/kotlin/StudyChallenge1.kt
@@ -3,8 +3,8 @@ fun StudyChallenge1() {
     var name: String = "Fernanda Freitas"
     var text: String = ""
     val footNumber: UInt = 36u
-    var brazilPIB: ULong = 189000000000u
+    var brazilPIB: ULong = 1869000000000u
     var brazilPopulation: ULong = 211000000u
 
-    println(brazilPIB)
+    println("R$ Brazil PIP per capita ${brazilPIB/brazilPopulation}")
 }
