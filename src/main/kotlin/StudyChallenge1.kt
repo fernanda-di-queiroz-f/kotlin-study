4172212f1af0f56b59d92070122a46a228459e33
Fernanda Di Queiroz Freitas <fernanda.diqueirozfreitas@thomsonreuters.com>
2022-05-21 17:40:51 -0300

    Added Challenge 1

---
+++ src/main/kotlin/StudyChallenge1.kt
@@ -0,0 +1,10 @@
+fun StudyChallenge1() {
+
+    var name: String = "Fernanda Freitas"
+    var text: String = ""
+    val footNumber: UInt = 36u
+    var brazilPIB: ULong = 189000000000u
+    var brazilPopulation: ULong = 211000000u
+
+    println(brazilPIB)
+}
